9449342fb46abc458c925a817e2de13f65b41604
Konstantin <konstantin.kaminskiy@outlook.com>
2024-09-29 16:07:20 -0400

    adds useLastVisitedPage to MapPage

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import GoogleMap from '../components/map.jsx';
 import DestinationPickList from '../components/destinationsPickList.jsx';
 import './css/MapPage.css';
+import useLastVisitedPage from '../hooks/useLastVisitedPage.js';
 
 const MapPage = (props) => {
+    useLastVisitedPage();
     const [destination, setDestination] = useState('');
 
     const updateDestination = (address) => {
