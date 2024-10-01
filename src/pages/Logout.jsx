8c6aa797458e5a2ceaaa461efde7829da99b2804
pepperrostami <pepper.rostami@gmail.com>
2024-10-01 12:11:19 -0400

    confirming that Profile.jsx is styled, responsive and code is cleaned up

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -6,12 +6,12 @@ function Logout(props) {
   props.logoutUser()
   return (
     <div className="logout-container">
-            <h3>YOU HAVE BEEN SUCCESSFULLY LOGGED OUT</h3>
-            <br></br>
-            <br></br>
-            <br></br>
-            <p>to re-log in, click <Link to="/" className="login-link">here</Link></p>
-        </div>
+      <h3>YOU HAVE BEEN SUCCESSFULLY LOGGED OUT</h3>
+      <br></br>
+      <br></br>
+      <br></br>
+      <p>to re-log in, click <Link to="/" className="login-link">here</Link></p>
+    </div>
   );
 }
 
